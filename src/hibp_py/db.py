e719c4e477bf3db9dc42ddb5e76861b0196ea5c1
Pavel Sushko <PSushko@sbsoftware.ca>
2024-03-27 13:46:56 -0400

    "Tyding up" (barely) the DB code

--- src/hibp_py/db.py
+++ src/hibp_py/db.py
@@ -1,33 +1,55 @@
 import json
 
 
+DATA_DIR = './data'
+USERS_FILE = f'{DATA_DIR}/users.json'
+BREACHES_FILE = f'{DATA_DIR}/breaches.json'
+
+
 def get_breaches(account):
+    """Gets all breaches for an account from the database
+
+    Args:
+        account (str): The account to check for breaches
+
+    Returns:
+        list: A list of breach names
     """
-    Get all breaches for an account
-    :param account: Account to check
-    :return: List of breaches
-    """
-    with open('./data/users.json', 'r') as f:
+    with open(USERS_FILE, 'r') as f:
         data = json.load(f)
 
     if account in data:
         return data[account]
 
-    return {}
+    return []
 
 
-def get_breach(id):
-    with open('./data/breaches.json', 'r') as f:
+def get_breach(breach_name):
+    """Gets a breach from the database
+
+    Args:
+        name (str): The name of the breach
+
+    Returns:
+        dict: A dictionary containing the breach data
+    """
+    with open(BREACHES_FILE, 'r') as f:
         data = json.load(f)
 
     if id in data:
-        return data[id]
+        return data[breach_name]
 
     return {}
 
 
 def write_breach(user, breach):
-    with open('./data/users.json', 'r') as f:
+    """Writes a breach to the database
+
+    Args:
+        user (str): The user to write the breach for
+        breach (dict): The breach data
+    """
+    with open(USERS_FILE, 'r') as f:
         data = json.load(f)
 
     if user not in data:
@@ -36,18 +58,14 @@ def write_breach(user, breach):
     elif breach['Name'] not in data[user]:
         data[user].append(breach['Name'])
 
-    with open('./data/users.json', 'w') as f:
+    with open(USERS_FILE, 'w') as f:
         json.dump(data, f, indent=4)
 
-    with open('./data/breaches.json', 'r') as f:
+    with open(BREACHES_FILE, 'r') as f:
         data = json.load(f)
 
     if breach['Name'] not in data:
         data[breach['Name']] = breach
 
-        with open('./data/breaches.json', 'w') as f:
+        with open(BREACHES_FILE, 'w') as f:
             json.dump(data, f, indent=4)
-
-
-if __name__ == '__main__':
-    print(get_breaches('mterry@ville.kirkland.qc.ca'))
